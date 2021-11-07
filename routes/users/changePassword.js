ce5c1a121474a2c9093fddd213b5ba265de6a764
Filippo Menis <filippomeniswork@gmail.com>
2021-11-07 20:41:55 +0100

    [users] change password API

---
+++ routes/users/changePassword.js
@@ -0,0 +1,67 @@
+import S from 'fluent-json-schema'
+import { compareStrings, hashString } from '../../lib/hash.js'
+
+export default async function changePassword(fastify, opts) {
+  const { db, config, httpErrors } = fastify
+
+  fastify.route({
+    method: 'POST',
+    path: '/:id/change-password',
+    config: {
+      public: false,
+    },
+    schema: {
+      tags: ['users'],
+      summary: 'Change password',
+      description: 'Change user password.',
+      params: S.object()
+        .additionalProperties(false)
+        .prop('id', S.integer().minimum(1))
+        .description('User id')
+        .required(),
+      body: S.object()
+        .additionalProperties(false)
+        .prop('old_pw', S.string().minLength(8))
+        .description('Current password')
+        .required()
+        .prop('new_pw', S.string().minLength(8))
+        .description('New password')
+        .required()
+        .prop('new_pw_confirmation', S.string().minLength(8))
+        .description('New password confirmation')
+        .required(),
+      response: {
+        200: fastify.getSchema('sNoContent'),
+        409: fastify.getSchema('sConflict')
+      }
+    },
+    handler: onChangePassword
+  })
+
+  async function onChangePassword(req, reply) {
+    const { old_pw, new_pw, new_pw_confirmation } = req.body
+    const { user } = req
+
+    const match = await compareStrings(old_pw, user.password)
+    if (!match) {
+      throw httpErrors.badRequest(`Old password not valid`)
+    }
+
+    if (new_pw === old_pw) {
+      throw httpErrors.badRequest(`New password is the same as the previous`)
+    }
+
+    if (new_pw !== new_pw_confirmation) {
+      throw httpErrors.badRequest(`The 'new' and 'confirmation' password does't match`)
+    }
+
+    const intpus = [user.id, await hashString(new_pw, parseInt(config.SALT_ROUNDS))]
+    const { rowCount } = await db.execQuery('UPDATE users SET password=$2 WHERE id=$1', intpus)
+
+    if (!rowCount) {
+      throw httpErrors.conflict('Password not updated')
+    }
+     
+    reply.code(204)
+  }
+}
--- routes/users/index.js
+++ routes/users/index.js
@@ -2,6 +2,7 @@ import createRoute from './create.js'
 import listRoute from './list.js'
 import readRoute from './read.js'
 import deleteRoute from './delete.js'
+import changePwRoute from './changePassword.js'
 
 export default async function index(fastify, opts) {
   const prefix = '/v1/users'
@@ -10,4 +11,5 @@ export default async function index(fastify, opts) {
   fastify.register(listRoute, { prefix })
   fastify.register(readRoute, { prefix })
   fastify.register(deleteRoute, { prefix })
+  fastify.register(changePwRoute, { prefix })
 }
