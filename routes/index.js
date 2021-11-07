c0160aa289a1faf9e3f57ade41e85615ea3168db
Filippo Menis <filippomeniswork@gmail.com>
2021-11-07 14:48:11 +0100

    [common] log body requests

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,20 @@ import miscRoutes from './misc/index.js'
 export default async function index(fastify, opts) {
   fastify.register(authentication)
 
+  fastify.addHook('preHandler', function (req, reply, done) {
+    if (req.body) {
+      if (req.body.password) {
+        req.log.info({ body: {
+          ...req.body,
+          password: '*'.repeat(req.body.password.length)
+        } }, 'parsed body')
+      } else {
+        req.log.info({ body: req.body }, 'parsed body')
+      }
+    }
+    done()
+  })
+
   fastify.addHook('onRoute', (routeOptions) => {
     routeOptions.schema = {
       ...routeOptions.schema,
