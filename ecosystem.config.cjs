e4e087b6c34f05ae17294802351bf68e8ac5f040
Filippo Menis <filippomeniswork@gmail.com>
2021-12-01 09:38:08 +0100

    [pm2] ecosystem file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'dilemma',
+      cwd: '/opt/dilemma/dilemma/dilemma/dilemma',
+      script: 'index.js',
+      node_args: '-r dotenv/config',
+      env: {
+        NODE_ENV: 'production',
+      },
+      exp_backoff_restart_delay: 100,
+      max_memory_restart: '500M',
+      max_restarts: 10,
+      error_file: '/var/log/dilemma/err.log',
+      out_file: '/var/log/dilemma/out.log',
+      log_file: '/var/log/dilemma/combined.log',
+    },
+  ],
+}
