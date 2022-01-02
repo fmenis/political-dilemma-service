b5f3774e7ce1bbd0295c72f66f329b71dcb6d822
Filippo Menis <filippomeniswork@gmail.com>
2022-01-03 00:26:56 +0100

    [regions] list API

--- routes/misc/index.js
+++ routes/misc/index.js
@@ -1,14 +1,16 @@
 import statusRoute from './status.js'
+import regionsRoute from './regions.js'
 
 export default async function index(fastify) {
   fastify.addHook('onRoute', options => {
     options.schema = {
       ...options.schema,
-      tags: ['misc']
+      tags: ['misc'],
     }
   })
 
   const prefix = '/v1'
 
   fastify.register(statusRoute, { prefix })
+  fastify.register(regionsRoute, { prefix })
 }
---
+++ routes/misc/regions.js
@@ -0,0 +1,38 @@
+import S from 'fluent-json-schema'
+
+export default async function listRegions(fastify) {
+  const { db } = fastify
+
+  fastify.route({
+    method: 'GET',
+    path: '/regions',
+    config: {
+      public: false,
+    },
+    schema: {
+      summary: 'Italian regions',
+      description: 'Get italian regions list.',
+      response: {
+        200: S.object().prop(
+          'results',
+          S.array().items(
+            S.object()
+              .additionalProperties(false)
+              .prop('id', S.integer().minimum(1))
+              .description('Region id')
+              .required()
+              .prop('name', S.string().minLength(3))
+              .description('Region name')
+              .required()
+          )
+        ),
+      },
+    },
+    handler: onListRegions,
+  })
+
+  async function onListRegions() {
+    const { rows } = await db.execQuery('SELECT * FROM regions')
+    return { results: rows }
+  }
+}
