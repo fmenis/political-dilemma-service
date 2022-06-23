7af0d6fc804c7bdbac2abc4e06aec616829abee8
Filippo Menis <filippomeniswork@gmail.com>
2022-06-23 20:26:26 +0200

    [roles] modify update API

--- src/routes/roles/crud/update.js
+++ src/routes/roles/crud/update.js
@@ -1,7 +1,7 @@
 import S from 'fluent-json-schema'
 
 import { sRoleResponse } from '../lib/schema.js'
-import { getRolePermissions } from '../lib/utils.js'
+import { getRolePermissions, associatePermissions } from '../lib/utils.js'
 
 export default async function updateRole(fastify) {
   const { pg, httpErrors } = fastify
@@ -28,7 +28,9 @@ export default async function updateRole(fastify) {
         .required()
         .prop('description', S.string().minLength(3).maxLength(200))
         .description('Role description.')
-        .required(),
+        .required()
+        .prop('permissionsIds', S.array().items(S.number()).minItems(1))
+        .description('Role permissions ids.'),
       response: {
         200: sRoleResponse(),
         404: fastify.getSchema('sNotFound'),
@@ -53,25 +55,48 @@ export default async function updateRole(fastify) {
 
   async function onUpdateRole(req) {
     const { id } = req.params
-    const { name, description } = req.body
+    const { name, description, permissionsIds } = req.body
 
-    const query =
-      'UPDATE roles SET name=$2, description=$3 WHERE id=$1' +
-      'RETURNING id, name, description, is_active'
+    let client
 
-    const { rowCount, rows } = await pg.execQuery(query, [
-      id,
-      name,
-      description,
-    ])
+    try {
+      client = await pg.beginTransaction()
 
-    if (!rowCount) {
-      throw httpErrors.conflict('The action had no effect')
+      const query =
+        'UPDATE roles SET name=$2, description=$3 WHERE id=$1' +
+        'RETURNING id, name, description, is_active'
+
+      const { rowCount, rows } = await pg.execQuery(
+        query,
+        [id, name, description],
+        { client }
+      )
+
+      if (!rowCount) {
+        throw httpErrors.conflict('The action had no effect')
+      }
+
+      if (permissionsIds?.length) {
+        // delete all role permissions
+        await pg.execQuery(
+          'DELETE FROM permissions_roles WHERE role_id=$1',
+          [id],
+          { client }
+        )
+
+        // re-assign all permissions
+        await associatePermissions(id, permissionsIds, pg, client)
+      }
+
+      await pg.commitTransaction(client)
+
+      const role = rows[0]
+      role.permissions = await getRolePermissions(id, pg)
+
+      return rows[0]
+    } catch (error) {
+      await pg.rollbackTransaction(client)
+      throw error
     }
-
-    const role = rows[0]
-    role.permissions = await getRolePermissions(id, pg)
-
-    return rows[0]
   }
 }
