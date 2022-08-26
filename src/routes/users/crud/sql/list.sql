dc71bc1c157eb3350202ff4e187d52add1855f53
Filippo Menis <filippomeniswork@gmail.com>
2022-08-27 00:23:47 +0200

    [users] fix list query

--- src/routes/users/crud/sql/list.sql
+++ src/routes/users/crud/sql/list.sql
@@ -3,8 +3,8 @@ SELECT
   users.id, users.first_name, users.last_name, users.user_name, users.email,
   users.joined_date, users.is_blocked, users.is_deleted, last_access,
   regions.name AS region, provinces.name AS province, roles.name AS role,
-  (SELECT count(id) FROM articles WHERE status = 'DRAFT') as "draftArticles",
-  (SELECT count(id) FROM articles WHERE status = 'PUBLISHED') as "publishedArticles"
+  (SELECT count(id) FROM articles WHERE status = 'DRAFT' AND "ownerId" = users.id) as "draftArticles",
+  (SELECT count(id) FROM articles WHERE status = 'PUBLISHED' AND "ownerId" = users.id) as "publishedArticles"
 {columns}
 FROM users
 JOIN provinces
@@ -15,5 +15,3 @@ JOIN users_roles
   ON users_roles.user_id = users.id
 JOIN roles
   ON roles.id = users_roles.role_id
-JOIN articles
-  ON articles."ownerId" = users.id
