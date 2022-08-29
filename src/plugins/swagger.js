025eb75983a1aa4292567c1df97de1ffbff5a486
Filippo Menis <filippomeniswork@gmail.com>
2022-08-29 22:05:09 +0200

    [common] from swagger2 to OpenApI

--- src/plugins/swagger.js
+++ src/plugins/swagger.js
@@ -2,29 +2,55 @@ import { readFileSync } from 'fs'
 import { join, resolve } from 'path'
 import fp from 'fastify-plugin'
 import Swagger from '@fastify/swagger'
+import { ENV } from '../common/enums.js'
 
 const { version } = JSON.parse(readFileSync(join(resolve(), 'package.json')))
 
 async function swaggerGenerator(fastify) {
+  const servers = [
+    {
+      url: `http://localhost:${process.env.SERVER_PORT}`,
+      description: 'Development server',
+      env: ENV.DEVELOPMENT,
+    },
+    //TODO
+    // {
+    //   url: `https://${process.env.DOMAIN_STAGING}`,
+    //   description: 'Staging server',
+    //   env: ENV.STAGING,
+    // },
+    {
+      url: `https://${process.env.DOMAIN_PROD}`,
+      description: 'Production server',
+      env: ENV.PRODUCTION,
+    },
+  ]
+
   fastify.register(Swagger, {
     routePrefix: '/doc',
-    swagger: {
+    openapi: {
       info: {
         title: 'Political Dilemma Service',
         description: 'Political Dilemma Service documentation',
         version,
+        contact: {
+          name: 'API Support',
+          email: 'filippomeniswork@gmail.com',
+        },
       },
       externalDocs: {
         url: 'https://github.com/fmenis/political-dilemma-service',
         description: 'Find more info here',
       },
-      host:
-        process.env.NODE_ENV === 'development'
-          ? `localhost:${process.env.SERVER_PORT}`
-          : process.env.DOMAIN_PROD,
-      schemes: ['http', 'https'],
-      consumes: ['application/json'],
-      produces: ['application/json'],
+      servers: servers.reduce((acc, item) => {
+        if (item.env === process.env.NODE_ENV) {
+          acc.push({
+            url: item.url,
+            description: item.description,
+          })
+        }
+        return acc
+      }, []),
       tags: [
         { name: 'auth', description: 'Auth related end-points' },
         { name: 'sessions', description: 'Session related end-points' },
@@ -44,7 +70,6 @@ async function swaggerGenerator(fastify) {
         { name: 'files', description: 'Files related end-points' },
       ].sort((a, b) => a.name.localeCompare(b.name)),
     },
-    // TODO passare a openApi v3
     exposeRoute: true,
   })
 }
