7c2d33949f9a44fd3a1ea43025fa9bd31a223cb5
Filippo Menis <filippomeniswork@gmail.com>
2022-10-05 23:36:54 +0200

    [common] update github actions

--- ecosystem-dev.config.cjs
+++ ecosystem-dev.config.cjs
@@ -5,15 +5,12 @@ module.exports = {
       cwd: '/opt/dilemma/dilemma/political-dilemma-service/political-dilemma-service',
       script: 'index.js',
       node_args: '-r dotenv/config',
-      env: {
-        NODE_ENV: 'production',
-      },
       exp_backoff_restart_delay: 100,
       max_memory_restart: '500M',
       max_restarts: 10,
-      error_file: '/var/log/dilemma/err.log',
-      out_file: '/var/log/dilemma/out.log',
-      log_file: '/var/log/dilemma/combined.log',
+      error_file: '/var/log/dilemma/dev/err.log',
+      out_file: '/var/log/dilemma/dev/out.log',
+      log_file: '/var/log/dilemma/dev/combined.log',
     },
   ],
 }
