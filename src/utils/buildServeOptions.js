82b09557488097eb7dbdde335a67be85bd6db316
Filippo Menis <filippomeniswork@gmail.com>
2022-11-01 20:46:59 +0100

    [common] improve logs

--- src/utils/buildServeOptions.js
+++ src/utils/buildServeOptions.js
@@ -5,6 +5,14 @@ export function buildServerOptions() {
     logger: {
       level: process.env.LOG_LEVEL,
       timestamp: () => stdTimeFunctions.isoTime(),
+      formatters: {
+        level(label) {
+          return { level: label }
+        },
+        bindings() {
+          return { pid: undefined, hostname: undefined }
+        },
+      },
       redact: {
         paths: ['password'],
         censor: '**GDPR COMPLIANT**',
