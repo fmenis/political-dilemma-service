a540f38fa07e7c53a4e6a967f17fefe1b9d6f39d
Filippo Menis <filippomeniswork@gmail.com>
2024-01-31 22:27:12 +0100

    improve dev-publish script

--- dev-publish.sh
+++ dev-publish.sh
@@ -4,20 +4,14 @@ SSH_KEY_PATH=$HOME/Documents/projects/political-dilemma/dev_politicaldilemma.pem
 REMOTE_HOST=13.39.48.132
 REMOTE_PATH=/opt/dev-dilemma
 REMOTE_USER=ubuntu
-FORCE=$1;
 
-# If the FORCE argument is not passed or not equal to "-f"
-# check the current branch and not trasfer the files if
-# it is not master
-# if [ -z "$FORCE" ] || [ $FORCE != "-f" ] ; then
-#     BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-#     if [[ "$BRANCH" != "master" ]]; then
-#         echo "Aborting: you are not in the master branch (current $BRANCH)";
-#         exit 1;
-#     fi
-# fi
+# Block if current branch is not develop
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [[ "$BRANCH" != "develop" ]]; then
+    echo "Aborting: you are not in the master branch (current $BRANCH)";
+    exit 1;
+fi
 
-echo  $SSH_KEY_PATH
 
 # Check ssh private key existence
 if [[ ! -f $SSH_KEY_PATH ]] ; then
@@ -26,7 +20,7 @@ if [[ ! -f $SSH_KEY_PATH ]] ; then
 fi
 
 # Send file to remote location
-rsync -vh \
+rsync -avhz \
       -e "ssh -i $SSH_KEY_PATH" \
       -r $PWD/ \
       --exclude=node_modules --exclude=logs --exclude=docs --exclude=.prettierrc --exclude=.git --exclude=.vscode \
@@ -37,4 +31,6 @@ rsync -vh \
       $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
 
 # Install dependencies and restart application process
-# ssh -i $SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_PATH; npm install; pm2 restart ecosystem-dev.config.cjs; pm2 save"
+ssh -i $SSH_KEY_PATH $REMOTE_USER@$REMOTE_HOST \
+"export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.11.0/bin; \
+cd $REMOTE_PATH; npm ci; npm run applyMigrations; pm2 startOrRestart ecosystem-dev.config.cjs; pm2 save"
