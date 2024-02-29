a44dd2db14b779ea7b0b3d1c73fe58787787c5f9
Filippo Menis <filippomeniswork@gmail.com>
2024-02-29 22:41:47 +0100

    update list and read user API output

--- src/routes/users/crud/sql/read.sql
+++ src/routes/users/crud/sql/read.sql
@@ -1,7 +1,8 @@
 SELECT 
   users.id, users.first_name, users.last_name, users.user_name, users.email, 
   users.type, users.id_region, users.id_province, users.bio, users.birth_date,  
-  users.joined_date, users.sex, users.is_blocked, is_deleted, users_roles.role_id
+  users.joined_date, users.sex, users.is_blocked, is_deleted, users_roles.role_id,
+  users.last_access
 FROM users  
 JOIN users_roles 
   ON users_roles.user_id = users.id
