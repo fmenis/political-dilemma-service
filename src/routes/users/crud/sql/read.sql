ef397d88cd0b48c24ec99a282dbdaad6925ed941
Filippo Menis <filippomeniswork@gmail.com>
2024-03-04 23:02:41 +0100

    revert query

--- src/routes/users/crud/sql/read.sql
+++ src/routes/users/crud/sql/read.sql
@@ -1,11 +1,9 @@
 SELECT 
   users.id, users.first_name, users.last_name, users.user_name, users.email, 
   users.type, users.id_region, users.id_province, users.bio, users.birth_date,  
-  users.joined_date, users.sex, users.is_blocked, is_deleted, users_roles.role_id, roles.name
+  users.joined_date, users.sex, users.is_blocked, is_deleted, users_roles.role_id,
   users.last_access
 FROM users  
 JOIN users_roles 
   ON users_roles.user_id = users.id
-JOIN roles 
-  ON users_roles.role_id = roles.id
 WHERE users.id = $1
