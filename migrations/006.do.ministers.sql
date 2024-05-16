46be4ee4ba6b243f0448a4d2f8d8fd49c3d27603
Filippo Menis <filippomeniswork@gmail.com>
2024-05-16 22:25:57 +0200

    fixies

--- migrations/006.do.ministers.sql
+++ migrations/006.do.ministers.sql
@@ -9,11 +9,13 @@ CREATE TABLE IF NOT EXISTS "legislature" (
 
 CREATE TABLE IF NOT EXISTS "ministry" (
     "id" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "name" VARCHAR(50) UNIQUE NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
     "legislatureId" UUID NOT NULL,
-    "politicianId" UUID UNIQUE NOT NULL,
+    "politicianId" UUID NOT NULL,
     "createdAt" timestamp DEFAULT NOW(),
     "updatedAt" timestamp DEFAULT NOW(),
+    UNIQUE ("name", "legislatureId"),
+    UNIQUE ("politicianId", "legislatureId"),
     CONSTRAINT fk_legislature_id FOREIGN KEY("legislatureId") REFERENCES "legislature"("id") ON DELETE NO ACTION,
     CONSTRAINT fk_politician_id FOREIGN KEY("politicianId") REFERENCES "politician"("id") ON DELETE NO ACTION
 );
